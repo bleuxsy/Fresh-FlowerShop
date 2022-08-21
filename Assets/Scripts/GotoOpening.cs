dc1b6dd56ab29adbadb054cc1c83d289d00519ea
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-08-22 07:24:23 +0900

    전체적인 빌드

---
+++ Assets/Scripts/GotoOpening.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GotoOpening : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Opening");
+    }
+}
