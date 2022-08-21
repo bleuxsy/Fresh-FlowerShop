81b72ba89f3f5e1185f9edaec50b08f5f92f9421
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-08-22 02:20:41 +0900

    종이, 리본, 이어하기, 오프닝
    MG-Paper (paper에서 이름 바꿈): 겉종이/속종이 토글 그룹으로 묶음. 위치 재배치함. 캔버스 설정 조정. Change (오른쪽 화살표) 스크립트 PtoR_change로 이름 바꿈. 기타 스프라이트 이름 조정.
    MG-Ribbon (Ribon select에서 이름 바꿈): 리본 토글 그룹으로 묶음. 위치 재배치함. 캔버스 설정 조정. LeftChange (왼쪽 화살표) 스크립트 RtoP_change로 이름 바꿈. 기타 스프라이트 이름 조정.
    Op-continue (Select에서 이름 바꿈): 위치 재배치함. 시작 화면으로 버튼 클릭하면 오프닝 화면으로 넘어가게 함. Opening 화면으로 이동하는 GotoOpening 스크립트 만들었음.
    Opening: 새로 생성함

---
+++ Assets/RtoP_change.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class RtoP_change : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("MG-Paper");
+    }
+}
