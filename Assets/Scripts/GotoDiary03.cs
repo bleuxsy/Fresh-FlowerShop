e6fc3d6561d70d9aaa361a6e9ab8f55e057c19e0
Seohyun Lee <32611398+seohyun-lee@users.noreply.github.com>
2022-08-23 04:26:27 +0900

    다이어리 작업+효과음추가

---
+++ Assets/Scripts/GotoDiary03.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GotoDiary03 : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("Diary03");
+    }
+}
